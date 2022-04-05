4c25fa7ef8b8a91ec4a3a0542cdf69e5f17d7875
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-04-04 23:57:52 -0500

    Graph shows

--- index.py
+++ index.py
@@ -8,10 +8,8 @@ weightedParams = []
 x = CSV.num(CSV.getCollum(csv, "compressionratio"))
 y = CSV.num(CSV.getCollum(csv, "price"))
 
-print(x)
-print(y)
-
 #https://towardsdatascience.com/master-machine-learning-multiple-linear-regression-from-scratch-with-python-ac716a9b78a4
 
 plt.figure()
-plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
+plt.plot(y)
+plt.show()
