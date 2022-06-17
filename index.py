bc6a39ced1d03efe59cb7cb725db7a4fba88f2d9
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-06-17 00:36:35 -0500

    Renamed index.py to oldindex.py and mushroomHandler.py to index.py

--- index.py
+++ index.py
@@ -1,90 +1,19 @@
-import CSV
-import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+df = pd.read_csv("./mushrooms.csv")
 
-csv = CSV.opencsv("./Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
+values = df.values
 
-csvHeaders = CSV.getHeaders(csv)
+t = np.vectorize(ord)
 
-weightedParams = [
-    # "symboling",
-    # "wheelbase",
-    # "carlength",
-    # "carwidth",
-    # "carheight",
-    # "curbweight",
-    # "enginesize",
-    # "boreratio",
-    # "stroke",
-    # "compressionratio",
-    # "horsepower",
-    # "peakrpm",
-    # "citympg",
-    # "highwaympg"
-]
-# yParam = "price"
+values = t(values)
 
-collums = []
-for collum in weightedParams:
-    collums.append(CSV.num(CSV.getCollum(csv, collum)))
+for i in range(1):
+    vals = values[:, i]
+    mn = vals.min()
+    mx = vals.max()
+    vals -= mn
+    values[:, i] = vals / int(mx-mn)
 
-weightCount = len(weightedParams)
-weights = []
-for i in range(weightCount):
-    weights.append(1)
-b = 1
-
-n = len(collums[0])
-itterations = 1000
-
-weightRange = range(weightCount)
-nRange = range(n)
-
-yCollum = CSV.num(CSV.getCollum(csv))
-
-alpha = 1  # Learning Rate
-
-errors = []
-
-def yHat(x):
-    v = b
-    for w in weights:
-        v += x*2
-    return v
-    
-
-for j in range(itterations):
-    averageerror = 0
-
-    # Bias
-    # err = 0
-    # for j in nRange:
-    #     y = yCollum[j]
-    #     err += 2 * (b - y)
-    # b -= alpha * err/n
-
-    for i in weightRange:
-        w = weights[i]
-        err = 0
-        collum = collums[i]
-
-        # Weights
-        for j in nRange:
-            x = collum[j]
-            y = yCollum[j]
-            err += (yHat(x) - y)*x
-
-        averageerror += err
-        # print("Offset: " + str(alpha * err / n))
-        weights[i] -= alpha * (err / n)
-    errors.append(averageerror/weightCount)
-
-plt.figure()
-plt.plot(errors)
-plt.show()
-
-
-# https://towardsdatascience.com/master-machine-learning-multiple-linear-regression-from-scratch-with-python-ac716a9b78a4
-
-# plt.figure()
-# plt.plot(y)
-# plt.show()
+print(values[:, 0])
+print(df.values[:, 0])
---
+++ oldindex.py
@@ -0,0 +1,90 @@
+import CSV
+import matplotlib.pyplot as plt
+
+csv = CSV.opencsv("./Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
+
+csvHeaders = CSV.getHeaders(csv)
+
+weightedParams = [
+    # "symboling",
+    # "wheelbase",
+    # "carlength",
+    # "carwidth",
+    # "carheight",
+    # "curbweight",
+    # "enginesize",
+    # "boreratio",
+    # "stroke",
+    # "compressionratio",
+    # "horsepower",
+    # "peakrpm",
+    # "citympg",
+    # "highwaympg"
+]
+# yParam = "price"
+
+collums = []
+for collum in weightedParams:
+    collums.append(CSV.num(CSV.getCollum(csv, collum)))
+
+weightCount = len(weightedParams)
+weights = []
+for i in range(weightCount):
+    weights.append(1)
+b = 1
+
+n = len(collums[0])
+itterations = 1000
+
+weightRange = range(weightCount)
+nRange = range(n)
+
+yCollum = CSV.num(CSV.getCollum(csv))
+
+alpha = 1  # Learning Rate
+
+errors = []
+
+def yHat(x):
+    v = b
+    for w in weights:
+        v += x*2
+    return v
+    
+
+for j in range(itterations):
+    averageerror = 0
+
+    # Bias
+    # err = 0
+    # for j in nRange:
+    #     y = yCollum[j]
+    #     err += 2 * (b - y)
+    # b -= alpha * err/n
+
+    for i in weightRange:
+        w = weights[i]
+        err = 0
+        collum = collums[i]
+
+        # Weights
+        for j in nRange:
+            x = collum[j]
+            y = yCollum[j]
+            err += (yHat(x) - y)*x
+
+        averageerror += err
+        # print("Offset: " + str(alpha * err / n))
+        weights[i] -= alpha * (err / n)
+    errors.append(averageerror/weightCount)
+
+plt.figure()
+plt.plot(errors)
+plt.show()
+
+
+# https://towardsdatascience.com/master-machine-learning-multiple-linear-regression-from-scratch-with-python-ac716a9b78a4
+
+# plt.figure()
+# plt.plot(y)
+# plt.show()
