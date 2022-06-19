b5e464367027aea99656c2ea6c183ac09e1d2fad
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-06-18 23:41:07 -0500

    Corrected spelling and removed debug print statements

--- index.py
+++ index.py
@@ -7,15 +7,9 @@ values = df.values
 t = np.vectorize(ord)
 
 values = t(values)
-values = values.astype(float)
 
-rows, collums = df.shape
-for i in range(collums):
-    vals = values[:, i]
-    mn = vals.min()
-    mx = vals.max()
-    vals -= mn
-    values[:, i] = vals / (mx-mn)
+rows, columns = df.shape
+for i in range(columns):
+    values[:, i] = pd.factorize(values[:, i])[:][0]
 
-    print(values[:, i])
-    print(df.values[:, i])
+xColumn = values[:, ]
