96f1f1d0fde6b61908c8b90fcadd6b3a07fb5590
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-06-20 00:41:43 -0500

    Removed zombie code from index.py

--- index.py
+++ index.py
@@ -22,31 +22,9 @@ rangesample = 1000
 xColumn = values[:rangesample, 1:].T
 yColumn = values[:rangesample, 0]
 
-# Creating test data
-# testweightcount = 2
-# for i in range(testweightcount):
-#     xColumn.append([])
-
-# for i in range(rangesample):
-#     val = .5
-#     for j in range(testweightcount):
-#         xColumn[j].append(random.random())
-#         val += xColumn[j][i] * j
-#     yColumn.append(val)
-# xColumn = np.array(xColumn)
-    
-
-
 b, weights = reg.regression(.005, 1000, yColumn, xColumn)
 
 plt.figure()
-
-# [[ Weight display]]
-# for i in range(len(weights)):
-#     x1 = [0, b]
-#     x2 = 1
-#     y2 = weights[i]
-#     plt.plot([0, 1], [b, weights[i]], marker="o")
 
 def clamp(val, minv, maxv):
     return min(max(minv, val), maxv)
